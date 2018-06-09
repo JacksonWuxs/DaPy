2b4974427d64abd86a58c1c517cf19a5fe492cc2
Xuansheng Wu <wuxsmail@163.com>
2018-06-09 19:44:10 +0800

    Version 1.3.3.1

---
+++ DaPy_Developing/core/__init__.py
@@ -0,0 +1,6 @@
+from basic_2d import SeriesSet, Frame, Table, Matrix
+from numeric import (cov, corr, frequency, quantiles, distribution,
+                     describe, mean)
+from basic_fun import is_seq, is_iter, is_math, is_value, get_sorted_index
+from numeric import _sum as sum
+from DataSet import DataSet
