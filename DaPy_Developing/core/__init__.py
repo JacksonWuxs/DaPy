c06a4631e72cb08135e4f2c302abff3be4f6a7a3
Xuansheng Wu <wuxsmail@163.com>
2018-06-29 13:48:45 +0800

    Delete __init__.py

--- DaPy_Developing/core/__init__.py
+++
@@ -1,5 +0,0 @@
-from base import SeriesSet, Frame, Table, Matrix
-from base import is_seq, is_iter, is_math, is_value, get_sorted_index
-from base import (cov, corr, frequency, quantiles, distribution, sum,
-                     describe, mean)
-from DataSet import DataSet
