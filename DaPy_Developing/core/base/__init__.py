28036b4bfd4e3edca485fc18c93fb6d51291cb5b
Xuansheng Wu <wuxsmail@163.com>
2018-06-29 14:18:04 +0800

    Delete __init__.py

--- DaPy_Developing/core/base/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._sheet import SeriesSet, Frame, Table, Matrix
-from ._function import is_seq, is_iter, is_math, is_value, get_sorted_index
-from ._numeric import (cov, corr, frequency, quantiles, distribution,
-                     describe, mean)
-from ._numeric import _sum as sum
